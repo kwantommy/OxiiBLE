553f286384a01024f91d40e4d93822fb39cb1d06
Tommy Kwan <tommykwan26@outlook.com>
2019-03-18 10:49:32 -0400

    100 HZ Sampling

---
+++ BLE Nano v2/Supplementary Code/TouchButton/TouchButton.ino
@@ -0,0 +1,28 @@
+/*
+Arduino     Touch Sensor
+  5V         VCC
+  GND        GND
+  D3         SIG
+*/
+
+int TouchSensor = 3; //connected to Digital pin D3
+int led = 2;
+void setup(){
+  Serial.begin(9600); // Communication speed
+  pinMode(led, OUTPUT);
+  pinMode(TouchSensor, INPUT);
+}
+
+void loop(){
+  if(digitalRead(TouchSensor)==HIGH)       //Read Touch sensor signal
+   { 
+    digitalWrite(led, HIGH);   // if Touch sensor is HIGH, then turn on
+    Serial.println("Led ON");
+   }
+  else
+   {
+    digitalWrite(led, LOW);    // if Touch sensor is LOW, then turn off the led
+    Serial.println("Led OFF");
+   }
+  delay(50); // Slow down the output for easier reading
+}
